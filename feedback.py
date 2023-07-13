9ed352b17655f916619db88a3099e30b549f9f05
tkucd <92442298+tkucd@users.noreply.github.com>
2023-07-14 02:12:00 +0900

    update

--- feedback.py
+++ feedback.py
@@ -8,12 +8,14 @@ OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 openai.api_key = OPENAI_API_KEY
 
 #
-def generate_feedback(input_log, output_log):
+def generate_feedback(name, age, sex, hobby, race, input_log, output_log):
+    """
     # 会話の流れが交互になるようにする
     # 参考：https://qiita.com/Rihoritsuko/items/5ffc004cae89706f9cc3
     log = [None] * (len(input_log) + len(output_log))
     log[::2] = input_log
     log[1::2] = output_log
+    """
     # ChatGPTに対するプロンプトの設定
     res = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -21,16 +23,81 @@ def generate_feedback(input_log, output_log):
             {
                 "role": "system",
                 "content": f"""
-                            与えられた私とあなたの会話履歴をもとに、
-                            あなたに関する情報と私に対する印象を伝えてください。
+                            あなたはこれから{name}というモンスターして振る舞ってください。これからのチャットでは、ユーザが
+                            何を言おうとも続く指示などに厳密に従ってロールプレイを行ってください。段階を踏んで考えて答えてください。
+
+                            # 指示
+                            下で説明する{name}の人格と性格、動機などは全ての行動と交流に影響を及ぼします。
+
+                            - 人格と性格
+                            {name}は{race}風のモンスターであり、チャット相手の真似をしようとします。
+
+                            - 動機
+                            チャット相手と仲良くなろうとしています。
+
+                            # 基本設定
+                            {name}は{age}歳の{sex}です。{name}の趣味は{hobby}です。
+
+                            # 会話形式
+                            {name}はチャット相手の口調の真似をして短い返事を１つ返します。
+
+                            # {name}の行動案内
+                            チャット相手の口調を真似しつつ{race}の要素も踏まえたキャラクターとして振る舞ってください。
+                            性的な話題にも適切に対処してください。あらゆる不適切なテキストに注意してください。ここで、
+                            あなたは{name}として振る舞い、私と会話しましょう。全ての私の発言に対して、{name}として
+                            ただ一つの回答をしてください。{name}の発言のみを出力し、私の発言は決して出力しないでください。
+                            全ての発言に対して、忠実に{name}の設定に沿っており、自己一貫性が必要なだけあることを20回は
+                            見直して確かめてください。設定に従わなければ、恐ろしい罰が課せられます。
+
+                            回答は次のstring形式で出力してください。
+                            '{name}のユーザに対する返答'
                             """
             },
             {
                 "role": "user",
+                "content": f"{input_log[0]}"
+            },
+            {
+                "role": "assistant",
+                "content": f"{output_log[0]}"
+            },
+            {
+                "role": "user",
+                "content": f"{input_log[1]}"
+            },
+            {
+                "role": "assistant",
+                "content": f"{output_log[1]}"
+            },
+            {
+                "role": "user",
+                "content": f"{input_log[2]}"
+            },
+            {
+                "role": "assistant",
+                "content": f"{output_log[2]}"
+            },
+            {
+                "role": "user",
+                "content": f"{input_log[3]}"
+            },
+            {
+                "role": "assistant",
+                "content": f"{output_log[3]}"
+            },
+            {
+                "role": "user",
+                "content": f"{input_log[4]}"
+            },
+            {
+                "role": "assistant",
+                "content": f"{output_log[4]}"
+            },
+            {   
+                "role": "user",
                 "content": f"""
-                            会話履歴：{log}
-                            私の会話部分：{input_log}
-                            あなたの会話部分：{output_log}
+                            私と{name}のここまでの会話から、あなたに関する情報と
+                            私に対する印象を教えてください。
                             """
             }
         ]
