1afc4182f10c7b9246cc46b82507a8b820dabd67
tkucd <92442298+tkucd@users.noreply.github.com>
2023-08-10 23:43:25 +0900

    update

--- stable.py
+++ stable.py
@@ -11,18 +11,23 @@ import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
 load_dotenv('.env')
 STABILITY_API_KEY = os.getenv('STABILITY_API_KEY')
 
-os.environ['STABILITY_HOST'] = 'grpc.stability.ai:443'
-
 stability_api = client.StabilityInference(
     key=STABILITY_API_KEY,
     verbose=True,
     engine="stable-diffusion-xl-1024-v0-9", 
 )
 
-def generate_image(filename, age, sex, hobby, race):
+def generate_image(filename, name, age, sex, hobby, race):
+    
+    img = Image.open(filename)
+    resized_img = img.resize((512, 512))
+
     res = stability_api.generate(
-        prompt="",
-        init_image=Image.open(filename),
+        prompt=f"""
+                この{name}というキャラクターを以下の情報をもとに超リアルに描いてください。
+                年齢：{age}、性別：{sex}、趣味：{hobby}、種族：{race}
+                """,
+        init_image=resized_img,
         start_schedule=0.8,
         seed=123463446,
         steps=50,
@@ -41,6 +46,10 @@ def generate_image(filename, age, sex, hobby, race):
             if artifact.type == generation.ARTIFACT_IMAGE:
                 global img2
                 img2 = Image.open(io.BytesIO(artifact.binary))
-                img2.save("shibainu-sk8-"+str(artifact.seed)+ "-img2img.png")
+                img2.save("a.png")
 
     return img2
+
+if __name__ == "__main__":
+    filename = "D:\MonsLabo_backend\Anime_shibainu-640x640.png"
+    generate_image(filename, "ライアン", 10, "男", "筋トレ", "星の妖精")
